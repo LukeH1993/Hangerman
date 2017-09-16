9c285314f96e6fa25573c8e446205085b6aeb96e
LukeH1993 <lwheudebourck@gmail.com>
2017-09-16 14:39:37 +0100

    Made changes to Javascript code, started to create hangman character

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 $ (function() {
 
+	//Variables
 	var words = ['hello', 'goodbye', 'sparta', 'global'];
 	var characters = null;
 
 	console.log(words.length);
-
 
 	characters = getRandomWord();
 	console.log(characters);
@@ -13,6 +13,7 @@ $ (function() {
 	 	$('.blank_spaces').append('<div class="blanks">_</div>');
 	}
 
+	// Displays a new hangman word at random on the screen.
 	function getRandomWord() {
 		var random = Math.floor(Math.random() * words.length);
 		return words[random];
@@ -20,12 +21,18 @@ $ (function() {
 
 	console.log(getRandomWord());
 
-	$('p').on('click', function() {
-		console.log('I work! YES!!!');
-	});
-
+	// Fades out instruction screen and displays the game screen.
 	$('#inst_button').on('click', function() {
 		$('.instructions').fadeOut();
 	});
 
+	// Displays letters on the screen when the user selects the letter buttons.
+	// var myButtons = document.getElementsByClassName('.letter');
+	// console.log(myButtons);
+
+	// for(var i = 0; i < myButtons.length; i++) {
+	// 	myButtons[i].addEventListener('click', function (event) {
+	// 		console.log('a');
+	// 	});
+	}
 })
