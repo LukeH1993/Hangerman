42ffeeec8adab1e252e5cf01c9f16005719f88bb
LukeH1993 <lwheudebourck@gmail.com>
2017-09-19 17:06:32 +0100

    MVP is complete

--- js/main.js
+++ js/main.js
@@ -9,10 +9,10 @@
 $ (function() {
 
 	//Variables
-	var words = ['petit fours', 'cheesy nachos', 'french fries', 'cheeseburger' , 'apple', 'fruit scone'];
+	var words = ['petit-fours', 'cheesy-nachos', 'french-fries', 'cheeseburger' , 'apple', 'fruit-scone'];
 	var characters = null;
 	var incorrect = 0;
-	var correct = null;
+	var correct = [];
 	var gameScore = 10;
 
 	//------------------------------------------------------------------------------------------------------------------------------
@@ -38,72 +38,73 @@ $ (function() {
 	game();
 	//------------------------------------------------------------------------------------------------------------------------------
 	function game() {
-		gameScore = 5;
-		if (gameScore > 0) {
 		$('.letter').on('click', function () {
 			if (characters.includes($(this).val())) {
 				for(var i = 0; i < characters.length; i++) {
 					if($(this).val() === characters[i]) {
 						$('.blanks').eq(i).html($(this).val());
+						correct.push($(this).val());
 						console.log($('.blanks').val());
-						gameScore++;
-						$('#new_score').html(gameScore);
+						//gameScore++;
+						//$('#new_score').html(gameScore);
 					}
 				}
 				console.log($(this).val());
-				if (correct === words.length) {
-					$('#losewin').html('YOU WIN!! Your final score is ' + gameScore).addClass('show');
+
+				if ($('.blanks:contains("_")').length === 0) {
+				
+					$('#losewin').html('YOU WIN!!').removeClass('hide');
 					$('.game_buttons').hide();
 				}
 			} else {
 				incorrect += 1;
 				showHangman();
 				console.log('incorrect: ' + incorrect);
-				gameScore--;
+				//gameScore--;
 				$(this).hide();
-				if(gameScore <= 0) {
-					gameScore = 0;
-					$('#losewin').html('GAME OVER! Your final score is ' + gameScore).addClass('show');
-					$('.game_buttons').hide();
-				} 
-				$('#new_score').html(gameScore);
+				// if(gameScore <= 0) {
+				// 	gameScore = 0;
+				// 	$('#losewin').html('GAME OVER! Your final score is ' + gameScore).addClass('show');
+				// 	$('.game_buttons').hide();
+				// } 
+				//$('#new_score').html(gameScore);
 			}
 		});
-			} else if (gameScore < 0) {
-				$('.game_buttons').hide();
-			}
+			// } else if (gameScore < 0) {
+			// 	$('.game_buttons').hide();
+			// }
 	}
 
 	//------------------------------------------------------------------------------------------------------------------------------
 	function showHangman() {
 		if (incorrect === 1) {
 			$('#pole').addClass('show');
-			gameScore--;
+			//gameScore--;
 		} else if (incorrect === 2) {
 			$('#pole_top').addClass('show');
-			gameScore--;
+			//gameScore--;
 		} else if (incorrect === 3) {
 			$('#rope').addClass('show');
-			gameScore--;
+			//gameScore--;
 		} else if (incorrect === 4) {
 			$('#head').addClass('show');
-			gameScore--;
+			//gameScore--;
 		} else if (incorrect === 5) {
 			$('#left_arm').addClass('show');
-			gameScore--;
+			//gameScore--;
 		} else if (incorrect === 6) {
 			$('#body').addClass('show');
-			gameScore--;
+			//gameScore--;
 		} else if (incorrect === 7) {
 			$('#right_arm').addClass('show');
-			gameScore--;
+			//gameScore--;
 		} else if (incorrect === 8) {
 			$('#left_leg').addClass('show');
-			gameScore--;
+			//gameScore--;
 		} else if (incorrect === 9) {
 			$('#right_leg').addClass('show');
-			gameScore--;
-			$('#losewin').html('GAME OVER! Your final score is ' + gameScore).addClass('show');
+			//gameScore--;
+			$('#losewin').html('GAME OVER!').addClass('show');
 		}
 	}
 	//------------------------------------------------------------------------------------------------------------------------------
